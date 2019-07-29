85163f3f1f2382613facf4f2e3343956047929e4
Jordi Salvador <jordi.salvador@launchmetrics.com>
2019-07-29 12:47:12 +0200

    Fixed folder configuration & added promises on publish function, also use request-promises instead of sync request

--- grafana/publish.js
+++ grafana/publish.js
@@ -19,72 +19,83 @@
 // THE SOFTWARE.
 
 'use strict';
-var request = require('request');
+var request = require('request-promise');
 var config = require('./config');
 var errors = require('./errors');
 
 /* eslint-disable max-statements, max-len, no-console, no-undef */
-function publish(dashboard, opts) {
-    opts = opts || {};
+async function publish(dashboard, opts) {
+    return new Promise(function (resolve, reject) {
+        opts = opts || {};
 
-    if (!dashboard) {
-        throw errors.UnfulfilledRequirement({
-            component: 'grafana.publish',
-            unfulfilledArg: 'dashboard'
-        });
-    }
-
-    var state = dashboard.state;
-    var cfg = config.getConfig();
-
-    if (!state || !state.title) {
-        throw errors.InvalidState({
-            component: 'grafana.Dashboard',
-            invalidArg: 'title',
-            reason: 'undefined'
-        });
-    }
-
-    if (!cfg.url) {
-        throw errors.Misconfigured({
-            invalidArg: 'url',
-            reason: 'undefined',
-            resolution: 'Must call grafana.configure before publishing'
-        });
-    }
-
-    if (!cfg.token) {
-        throw errors.Misconfigured({
-            invalidArg: 'token',
-            reason: 'Token is required for API utilization'
-        });
-    }
-
-    var createData = {
-        dashboard: dashboard.generate(),
-        folderId: dashboard.getFolderId(opts),
-        overwrite: true
-    };
-
-    request({
-        url: cfg.url + '/api/dashboards/db/',
-        method: 'POST',
-        json: createData,
-        headers: {
-            "Authorization": "Bearer " + cfg.token
-        },
-        timeout: opts.timeout || 1000
-    }, function createResponseHandler(createErr, createResp) {
-        if (createErr) {
-            console.log('Unable to publish dashboard: ' + createErr);
-        } else if ([200, 201].indexOf(createResp.statusCode) === -1) {
-            console.log('Unable to publish dashboard ' + state.title);
-            console.log('Got statusCode' + createResp.statusCode);
-        } else {
-            console.log('Published the dashboard ' + state.title);
+        if (!dashboard) {
+            reject(errors.UnfulfilledRequirement({
+                component: 'grafana.publish',
+                unfulfilledArg: 'dashboard'
+            }));
         }
+
+        var state = dashboard.state;
+        var cfg = config.getConfig();
+
+        if (!state || !state.title) {
+            reject(errors.InvalidState({
+                component: 'grafana.Dashboard',
+                invalidArg: 'title',
+                reason: 'undefined'
+            }));
+        }
+
+        if (!cfg.url) {
+            reject(errors.Misconfigured({
+                invalidArg: 'url',
+                reason: 'undefined',
+                resolution: 'Must call grafana.configure before publishing'
+            }));
+        }
+
+        if (!cfg.token) {
+            reject(errors.Misconfigured({
+                invalidArg: 'token',
+                reason: 'Token is required for API utilization'
+            }));
+        }
+
+        var createData = {
+            dashboard: dashboard.generate(),
+            folderId: dashboard.getFolderId(opts),
+            overwrite: true
+        };
+
+        request({
+            uri: cfg.url + '/api/dashboards/db/',
+            method: 'POST',
+            body: createData,
+            headers: {
+                "Authorization": "Bearer " + cfg.token
+            },
+            json: true,
+            timeout: opts.timeout || 1000
+        }).then(createResp => {
+            if (createResp.status !== 'success') {
+                reject(errors.PublishError({
+                    dashboard: state.title,
+                    error: createResp.body.message,
+                    response: createResp.statusCode
+                }));
+            } else {
+                resolve(state);
+            }
+        }).catch(createErr => {
+            reject(errors.PublishError({
+                dashboard: state.title,
+                error: createErr.error.message,
+                response: JSON.stringify(createErr)
+            }));
+        });
     });
 }
-/* eslint-enable */
 
+
+/* eslint-enable */
 module.exports = publish;
